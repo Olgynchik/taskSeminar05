f076c641b0f92ef3c919ac3b8f8031d1a74eaa69
Olga <sinickaya1986@gmail.com>
2023-05-27 18:23:22 +0300

    second solution

--- task34/Program.cs
+++ task34/Program.cs
@@ -1,21 +1,46 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
-System.Console.WriteLine("Введите размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] mass = new int[size];
-Random rand = new Random();
-for (int i = 0; i < size; i++)
+// System.Console.WriteLine("Введите размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] mass = new int[size];
+// Random rand = new Random();
+// for (int i = 0; i < size; i++)
+// {
+//     mass[i] = rand.Next(100, 1000);
+// }
+// System.Console.WriteLine(string.Join(" ", mass));
+// int count = 0;
+// for (int i = 0; i < size; i++)
+// {
+//     if (mass[i] % 2 == 0)
+//     {
+//         count ++; // count = count + 1
+//     }
+// }
+// System.Console.WriteLine($"Количество четных чисел в массиве: {count} ");
+
+int Text(string text)
 {
-    mass[i] = rand.Next(100, 1000);
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
-System.Console.WriteLine(string.Join(" ", mass));
+int size = Text("Введите размер массива:");
 int count = 0;
-for (int i = 0; i < size; i++)
+int[] Massiv(int size, int leftRange, int rightRange)
 {
-    if (mass[i] % 2 == 0)
+    int[] mass = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
     {
-        count ++; // count = count + 1
+        mass[i] = rand.Next(leftRange, rightRange);
+        if (mass[i] % 2 == 0)
+        {
+            count++; // count = count + 1
+        } 
     }
+    return mass;  
 }
-System.Console.WriteLine($"Количество четных чисел в массиве: {count} ");
+int[] Array = Massiv(size, 100, 1000);
+System.Console.WriteLine(string.Join(" ", Array));
+System.Console.WriteLine($"Количество четных чисел в массиве: {count} "); 
